233400bac34e23535bf1c15648d6d2911f881dd2
Márton Braun <marton.braun@jetbrains.com>
2023-11-17 10:32:50 +0100

    Experiment to use Amper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -16,6 +17,10 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
+}
+
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.1.0")
 }
 
 include(":composeApp")
