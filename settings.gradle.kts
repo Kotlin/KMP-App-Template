cb5c4aadd87e92fe73258aeb7553db50d2100e20
Márton Braun <marton.braun@jetbrains.com>
2024-05-08 14:17:39 +0200

    Update versions, sync changes from KMP wizard

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,15 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        google()
-        gradlePluginPortal()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
 
         // Amper repositories
         maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
@@ -17,9 +22,14 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
