7007053eeb85ac9ba4a49a4b08e0a904b06f760e
Márton Braun <marton.braun@jetbrains.com>
2024-09-04 10:06:44 +0200

    Update dependency versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
