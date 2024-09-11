55d604bd3db0b1eb2e4cc2a54026f3efdec61b02
Márton Braun <marton.braun@jetbrains.com>
2024-09-11 18:10:31 +0200

    Remove unused preview

--- composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +13,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
