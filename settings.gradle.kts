295ce86a8572226fcf44a01ec6d971243fba793c
Márton Braun <marton.braun@jetbrains.com>
2024-09-13 17:01:37 +0200

    Update Gradle wrapper to 8.10.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
         gradlePluginPortal()
 
         // Amper repositories
-        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
         maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
@@ -34,5 +34,5 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.3.0")
+    id("org.jetbrains.amper.settings.plugin").version("0.4.0")
 }
