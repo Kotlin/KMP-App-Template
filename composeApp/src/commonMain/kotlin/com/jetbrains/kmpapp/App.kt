d60e85c0b952fcaca768ba85fc38a97f20c8ebbe
Márton Braun <marton.braun@jetbrains.com>
2024-11-12 13:26:34 +0100

    Sort imports

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
@@ -3,8 +3,8 @@ package com.jetbrains.kmpapp
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
