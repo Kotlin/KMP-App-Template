07c0bd60b2086967507356ad252b2eb598c8577d
Márton Braun <marton.braun@jetbrains.com>
2025-12-01 14:36:19 +0100

    Fix Swift imports

--- iosApp/src/ContentView.swift
+++ iosApp/src/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import kotlin
+import KotlinModules
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
--- iosApp/src/iOSApp.swift
+++ iosApp/src/iOSApp.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import kotlin
+import KotlinModules
 
 @main
 struct iOSApp: App {
