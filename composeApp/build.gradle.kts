193f5b5253795352fbabcb5c8be7fa52a8250440
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2026-01-21 12:26:25 +0100

    Koin Annotations for Koin Plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,6 +57,7 @@ kotlin {
             implementation(libs.coil.compose)
             implementation(libs.coil.network.ktor)
             implementation(libs.koin.compose.viewmodel)
+            implementation(libs.koin.annotations)
         }
     }
 }
