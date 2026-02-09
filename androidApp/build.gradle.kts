eea7e9e544c372f77facabf137f1311fb4c950f1
Márton Braun <marton.braun@jetbrains.com>
2026-02-09 17:50:14 +0100

    Enable built-in Kotlin

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.androidBuiltInKotlin)
     alias(libs.plugins.composeCompiler)
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.androidKmpLibrary) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.composeMultiplatform) apply false
-    alias(libs.plugins.androidBuiltInKotlin) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
 }
